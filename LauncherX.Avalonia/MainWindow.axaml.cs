9b7b4532031387234f6c6922763360fc4303a3fa
Matthias Wang <matthiaswang60@gmail.com>
2022-01-01 17:48:14 +0800

    Added Dialog for 'Add a website' button

--- LauncherX.Avalonia/MainWindow.axaml.cs
+++ LauncherX.Avalonia/MainWindow.axaml.cs
@@ -1,7 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using FluentAvalonia.UI.Controls;
+using LauncherX.Avalonia.Pages;
 
 namespace LauncherX.Avalonia
 {
@@ -18,6 +20,22 @@ namespace LauncherX.Avalonia
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        public async void AddWebsiteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //show a contentdialog with a textbox to enter the website URL
+            
+            //init contentdialog
+            ContentDialog WebsiteDialog = new ContentDialog();
+            WebsiteDialog.Title = "Add a website";
+            WebsiteDialog.PrimaryButtonText = "OK";
+            WebsiteDialog.CloseButtonText = "Cancel";
+            WebsiteDialog.DefaultButton = ContentDialogButton.Primary;
+            WebsiteDialog.Content = new WebsiteDialogContentPage();
+
+            var result = await WebsiteDialog.ShowAsync();
+
         }
     }
 }
