3163babd9e1dbd1f65f70551ec21420494511eff
Matthias Wang <matthiaswang60@gmail.com>
2024-08-03 19:40:01 +0800

    Begin migration to WinUI3 - MainWindow.xaml basic UI

---
+++ LauncherXWinUI/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace LauncherXWinUI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : WinUIEx.WindowEx
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Container_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Set placeholder titlebar for now, before WASDK 1.6
+            this.ExtendsContentIntoTitleBar = true;
+            this.SetTitleBar(AppTitleBar);
+
+
+            // Configure either Mica or Acrylic background, depending on which is available
+            if (Microsoft.UI.Composition.SystemBackdrops.MicaController.IsSupported())
+            {
+                Microsoft.UI.Xaml.Media.MicaBackdrop micaBackdrop = new Microsoft.UI.Xaml.Media.MicaBackdrop();
+                micaBackdrop.Kind = Microsoft.UI.Composition.SystemBackdrops.MicaKind.Base;
+                this.SystemBackdrop = micaBackdrop;
+
+            }
+            else if (Microsoft.UI.Composition.SystemBackdrops.DesktopAcrylicController.IsSupported())
+            {
+                Microsoft.UI.Xaml.Media.DesktopAcrylicBackdrop DesktopAcrylicBackdrop = new Microsoft.UI.Xaml.Media.DesktopAcrylicBackdrop();
+                this.SystemBackdrop = DesktopAcrylicBackdrop;
+                ContainerFallbackBackground.Opacity = 0.6;
+            }
+            else
+            {
+                // Load fallback background colour
+                ContainerFallbackBackground.Opacity = 1.0;
+            }
+        }
+    }
+}
