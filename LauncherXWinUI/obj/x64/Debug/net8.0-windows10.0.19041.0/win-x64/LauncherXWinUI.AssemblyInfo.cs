a4e84eefcfb25924ab72b1b6fae7df457ae46dfb
Matthias Wang <matthiaswang60@gmail.com>
2024-08-18 00:50:21 +0800

    Add third party notices and licenses

--- LauncherXWinUI/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/LauncherXWinUI.AssemblyInfo.cs
+++ LauncherXWinUI/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/LauncherXWinUI.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2020-present ClickPhase")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("LauncherX - Modern app launcher for Windows")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.1.0.0+2547b5f67a1daa8cf2467751258da4f907357d33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.1.0.0+933b98c5d86652ec1f8c9fac19fffd7d3b24c1c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("LauncherX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LauncherX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.1.0.0")]
